020267c4c33b551c5cc0cc362968b6065b4a8331
DanielK <rafikrafikrafik@yandex.ru>
2018-09-30 10:48:22 +0300

    Remove api key

--- settings.py
+++ settings.py
@@ -1,5 +1,3 @@
 PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
          'urllib3_proxy_kwargs': {'username': 'learn',
                                   'password': 'python'}}
-
-API_KEY = '595174097:AAFgsePGJgbdL7alGsABjQ4IKxPTC5sH58o'
