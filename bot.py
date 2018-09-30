febf883151f8c3a3038c5a0806666ac21a6b5f94
DanielK <rafikrafikrafik@yandex.ru>
2018-09-30 18:20:31 +0300

    Add logging to top lvl module

--- bot.py
+++ bot.py
@@ -3,9 +3,15 @@ import os
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, RegexHandler, ConversationHandler
 
 from settings import PROXY
-from handlers import start_handler
+from handlers import *
 
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+                    )
 
+logger = logging.getLogger(__name__)
+logger.info('BOTBOT')
 
 
 
@@ -13,7 +19,11 @@ def main():
     mybot = Updater(os.getenv('API_KEY'), request_kwargs=PROXY)
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', start_handler, pass_user_data=True))
+    dp.add_handler(pizza_menu)
 
+
+    mybot.start_polling()
+    mybot.idle()
 
 
 if __name__ == '__main__':
