c446e5e678ea699e67953bdc178dbcf5c852d3c2
DanielK <rafikrafikrafik@yandex.ru>
2018-10-12 10:20:27 +0300

    Some refactoring; add functioning checkout menu, where you can change ur order; add send order menu, where ur name and phone are asked and ur order is sent to chat with admin (me for now)

--- handlers.py
+++ handlers.py
@@ -4,17 +4,18 @@ from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
 from telegram.ext import MessageHandler, CommandHandler, ConversationHandler, RegexHandler, Filters
 
 from pizza_handlers import (pizza_main_menu_handler, menu_button_handler, special_offers_handler,
-    checkout_handler, contact_info_handler, pizza_category_handler, drinks_category_handler,
-    other_category_handler, back_from_menu_handler, add_pizza_to_order_handler,
-    change_menu_page_handler
-)
+                            checkout_handler, contact_info_handler, pizza_category_handler, drinks_category_handler,
+                            other_category_handler, back_from_menu_handler, add_pizza_to_cart_handler,
+                            change_menu_page_handler, back_from_checkout_handler, change_cart_handler,
+                            order_pizza_handler, back_from_order_handler, send_order_handler, 
+                            remove_from_cart_handler, back_from_removing_from_cart_handler
+                            )
 
 
 def start_handler(bot, update, user_data):
-    update.message.reply_text(''' Привет, я бот, который поможет
-тебе заказать пиццу или выпить кофе!
-Выбери опцию, нажав одну из кнопок:''',
-         reply_markup = ReplyKeyboardMarkup([['Pizza main menu'],['Cafe main menu'],['Test_phone_and_name']]))
+    update.message.reply_text('Привет, я бот, который поможет\nтебе заказать пиццу или выпить кофе!\n'+
+        'Выбери опцию, нажав одну из кнопок:', reply_markup=ReplyKeyboardMarkup([
+            ['Pizza main menu'], ['Cafe main menu']]))
     return 'mode_choise_state'
 
 
@@ -24,140 +25,166 @@ def cafe_main_menu_handler(bot, update, user_data):
 
 
 def end_handler(bot, update, user_data):
-    update.message.reply_text('End state!', reply_markup = ReplyKeyboardRemove())
-    return ConversationHandler.END 
+    update.message.reply_text('End state!', reply_markup=ReplyKeyboardRemove())
+    return ConversationHandler.END
 
 
 def cancel_handler(bot, update, user_data):
-    update.message.reply_text('You have canceled the conversation', reply_markup = ReplyKeyboardRemove()) 
+    update.message.reply_text('You have canceled the conversation', reply_markup=ReplyKeyboardRemove())
 
 
-def test_name_and_phone(bot, update, user_data):
-    phone_button = KeyboardButton('Телефон', request_contact = True)
-    update.message.reply_text('Выберите опцию:',reply_markup = ReplyKeyboardMarkup([[phone_button],['Ввести имя']]))
-    return 'test_phone_choise'
+def enter_name_and_phone_handler(bot, update, user_data):
+    phone_button = KeyboardButton('Телефон', request_contact=True)
+    update.message.reply_text('Пожалуйста, заполните данные о себе:',
+        reply_markup=ReplyKeyboardMarkup([[phone_button], ['Ввести имя'],['Назад']]))
+    return 'enter_phone_choise'
 
 
-def test_phone(bot, update, user_data):
+def enter_phone_handler(bot, update, user_data):
     if 'phone' in user_data.keys():
         update.message.reply_text(f'Я знаю, что ваш Телефон - {user_data["phone"]}')
-        return ConversationHandler.END
+        return enter_name_and_phone_handler(bot, update, user_data)
     else:
-        update.message.reply_text(f'Ваш Телефон - {update.message.contact["phone_number"]}?',reply_markup = ReplyKeyboardMarkup([['Yes'],['No']]))
+        update.message.reply_text(f'Ваш Телефон - {update.message.contact["phone_number"]}?',
+                                  reply_markup=ReplyKeyboardMarkup([['Yes'], ['No']]))
         user_data['temp'] = update.message.contact['phone_number']
     return 'phone_choise'
 
 
-def phone_good(bot, update, user_data):
+def phone_good_handler(bot, update, user_data):
     user_data['phone'] = user_data['temp']
     update.message.reply_text(f'Отлично, я запомню его {user_data["phone"]}')
-    return ConversationHandler.END
+    return check_user_data_completeness(bot, update, user_data)
 
 
-def phone_bad(bot, update, user_data):
+def phone_bad_handler(bot, update, user_data):
     update.message.reply_text('Тогда введите его в формате 79001234567:')
     return 'phone_input'
 
 
-def check_phone_input(bot, update, user_data):
+def check_phone_input_handler(bot, update, user_data):
     user_input = update.message.text.lstrip().rstrip()
     print(user_input)
-    phone_to_check = re.findall(r'^\d{11}$',user_input)
+    phone_to_check = re.findall(r'^\d{11}$', user_input)
     print(phone_to_check)
-    if len(phone_to_check)!=1:
+    if len(phone_to_check) != 1:
         update.message.reply_text('Проверьте формат ввода и введие снова')
         return 'phone_input'
     else:
         user_data['phone'] = user_input
         update.message.reply_text(f'Отлично, я запомню его {user_data["phone"]}')
-        return ConversationHandler.END
+        return enter_name_and_phone_handler(bot, update, user_data)
 
 
-def test_name(bot, update, user_data):
+def enter_name_handler(bot, update, user_data):
     if 'name' in user_data.keys():
         update.message.reply_text(f'Я знаю, что вас зовут - {user_data["name"]}')
-        return ConversationHandler.END
+        return enter_name_and_phone_handler(bot, update, user_data)
     else:
-        update.message.reply_text(f'Вас зовут - {update.message.from_user.full_name}?',reply_markup = ReplyKeyboardMarkup([['Yes'],['No']]))
-#        user_data['temp'] = update.message.contact['phone_number']
+        update.message.reply_text(f'Вас зовут - {update.message.from_user.full_name}?',
+                                  reply_markup=ReplyKeyboardMarkup([['Yes'], ['No']]))
     return 'name_choise'
 
 
-def name_good(bot, update, user_data):
+def name_good_handler(bot, update, user_data):
     user_data['name'] = update.message.from_user.full_name
     update.message.reply_text(f'Отлично, я запомню его {user_data["name"]}')
-    return ConversationHandler.END
+    return check_user_data_completeness(bot, update, user_data)
 
 
-def name_bad(bot, update, user_data):
+def name_bad_handler(bot, update, user_data):
     update.message.reply_text('Тогда введите его в формате Имя Фамилия:')
     return 'name_input'
 
 
-def check_name_input(bot, update, user_data):
+def check_name_input_handler(bot, update, user_data):
     user_input = update.message.text.lstrip().rstrip()
     print(user_input)
-    name_to_check = re.findall(r'\w+ \w+',user_input)       #TODO - replace \w with smth
+    name_to_check = re.findall(r'\w+ \w+', user_input)  # TODO - replace \w with smth
     print(name_to_check)
-    if len(name_to_check)!=1:
+    if len(name_to_check) != 1:
         update.message.reply_text('Проверьте формат ввода и введие снова')
         return 'name_input'
     else:
         user_data['name'] = user_input
         update.message.reply_text(f'Отлично, я запомню его {user_data["name"]}')
-        return ConversationHandler.END
+        return enter_name_and_phone_handler(bot, update, user_data)
 
 
+def check_user_data_completeness(bot, update, user_data):
+    if 'phone' in user_data.keys() and 'name' in user_data.keys():
+        return 'pizzeria_send_order'
+    else:
+        return enter_name_and_phone_handler(bot, update, user_data)
 
 
 conversation = ConversationHandler(
-    entry_points = [CommandHandler('start', start_handler, pass_user_data = True)],
-    states = {
-        'mode_choise_state':[
-            RegexHandler('^(Pizza main menu)$', pizza_main_menu_handler, pass_user_data = True),
-            RegexHandler('^(Cafe main menu)$', cafe_main_menu_handler, pass_user_data = True),
-            RegexHandler('^(Test_phone_and_name)$', test_name_and_phone, pass_user_data = True)
+    entry_points=[CommandHandler('start', start_handler, pass_user_data=True)],
+    states={
+        'mode_choise_state': [
+            RegexHandler('^(Pizza main menu)$', pizza_main_menu_handler, pass_user_data=True),
+            RegexHandler('^(Cafe main menu)$', cafe_main_menu_handler, pass_user_data=True),
+            RegexHandler('^(enter_phone_and_name)$', enter_name_and_phone_handler, pass_user_data=True)
         ],
-        'state_choise':[
-            RegexHandler('^(Menu_button)$', menu_button_handler, pass_user_data = True),
-            RegexHandler('^(Special_offers)$', special_offers_handler, pass_user_data = True),
-            RegexHandler('^(Checkout)$', checkout_handler, pass_user_data = True),
-            RegexHandler('^(Contact_info)$', contact_info_handler, pass_user_data = True)
-            ],
-        'menu_state':[
-            RegexHandler('^(Пицца)$', pizza_category_handler, pass_user_data = True),
-            RegexHandler('^(Напитки)$', drinks_category_handler, pass_user_data = True),
-            RegexHandler('^(Прочее)$', other_category_handler, pass_user_data = True),
-            RegexHandler('^(Назад)$', back_from_menu_handler, pass_user_data = True)
+
+        'pizzeria_main_menu_state': [
+            RegexHandler('^(Menu_button)$', menu_button_handler, pass_user_data=True),
+            RegexHandler('^(Special_offers)$', special_offers_handler, pass_user_data=True),
+            RegexHandler('^(Checkout)$', checkout_handler, pass_user_data=True),
+            RegexHandler('^(Contact_info)$', contact_info_handler, pass_user_data=True)
         ],
-        'pizza_choise':[
-            RegexHandler('^\d+$', add_pizza_to_order_handler, pass_user_data = True),
-            RegexHandler('^Пред\.|След\.|Назад$', change_menu_page_handler, pass_user_data = True)
+        'pizzeria_menu_state': [
+            RegexHandler('^(Пицца)$', pizza_category_handler, pass_user_data=True),
+            RegexHandler('^(Напитки)$', drinks_category_handler, pass_user_data=True),
+            RegexHandler('^(Прочее)$', other_category_handler, pass_user_data=True),
+            RegexHandler('^(Назад)$', back_from_menu_handler, pass_user_data=True)
         ],
-        'test_phone_choise':[
-            MessageHandler(Filters.contact, test_phone, pass_user_data=True),
-            RegexHandler('^(Ввести имя)$', test_name, pass_user_data = True)
+        'pizza_choise_state': [
+            RegexHandler('^\d+$', add_pizza_to_cart_handler, pass_user_data=True),
+            RegexHandler('^Пред\.|След\.|Назад$', change_menu_page_handler, pass_user_data=True)
         ],
-        'phone_choise':[
-            RegexHandler('^(Yes)$', phone_good, pass_user_data=True),
-            RegexHandler('^(No)$', phone_bad, pass_user_data=True),
+
+        'pizzeria_checkout_state':[
+            RegexHandler('^(Назад)$', back_from_checkout_handler, pass_user_data=True),
+            RegexHandler('^(Изменить заказ)$', change_cart_handler, pass_user_data=True),
+            RegexHandler('^Сделать заказ$', order_pizza_handler, pass_user_data=True)
         ],
-        'phone_input':[
-            MessageHandler(Filters.text, check_phone_input, pass_user_data = True)
+        'removeing_from_cart_state': [
+            RegexHandler('^\w+x\w\s*-1$', remove_from_cart_handler, pass_user_data=True),
+            RegexHandler('^(Назад)$', back_from_removing_from_cart_handler, pass_user_data=True),
         ],
-        'name_choise':[
-            RegexHandler('^(Yes)$', name_good, pass_user_data=True),
-            RegexHandler('^(No)$', name_bad, pass_user_data=True),
-        ],        
-        'name_input':[
-            MessageHandler(Filters.text, check_name_input, pass_user_data = True)
-        ],  
-        'end' :[
-        MessageHandler(Filters.text, end_handler, pass_user_data = True)
+
+        'pizzeria_make_order_state': [
+            RegexHandler('^(Назад)$', back_from_order_handler, pass_user_data=True),
+            RegexHandler('^(Отправить заказ)$', enter_name_and_phone_handler, pass_user_data=True)
+        ],
+        'pizzeria_send_order': [
+            MessageHandler(Filters.text, send_order_handler, pass_user_data=True)
+        ],
+
+        'enter_phone_choise': [
+            MessageHandler(Filters.contact, enter_phone_handler, pass_user_data=True),
+            RegexHandler('^(Ввести имя)$', enter_name_handler, pass_user_data=True),
+            RegexHandler('^(Назад)$', back_from_menu_handler, pass_user_data=True)
+        ],
+        'phone_choise': [
+            RegexHandler('^(Yes)$', phone_good_handler, pass_user_data=True),
+            RegexHandler('^(No)$', phone_bad_handler, pass_user_data=True),
+        ],
+        'phone_input': [
+            MessageHandler(Filters.text, check_phone_input_handler, pass_user_data=True)
+        ],
+        'name_choise': [
+            RegexHandler('^(Yes)$', name_good_handler, pass_user_data=True),
+            RegexHandler('^(No)$', name_bad_handler, pass_user_data=True),
+        ],
+        'name_input': [
+            MessageHandler(Filters.text, check_name_input_handler, pass_user_data=True)
+        ],
+
+        'end': [
+            MessageHandler(Filters.text, end_handler, pass_user_data=True)
         ]
     },
-    fallbacks = [CommandHandler('cancel', cancel_handler, pass_user_data = True)]
-    )
-
-
-
+    fallbacks=[CommandHandler('cancel', cancel_handler, pass_user_data=True)]
+)
--- pizza_handlers.py
+++ pizza_handlers.py
@@ -1,93 +1,169 @@
 from glob import glob
 import re
 
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
 from telegram.ext import MessageHandler, CommandHandler, ConversationHandler, RegexHandler, Filters
 
-
 pizza_main_menu_markup = ReplyKeyboardMarkup([['Menu_button'],
-                                     ['Special_offers'],
-                                     ['Checkout'],
-                                     ['Contact_info']]) 
+                                              ['Special_offers'],
+                                              ['Checkout'],
+                                              ['Contact_info']])
+
+from settings import ADMIN_ID
+
 
 
 def pizza_main_menu_handler(bot, update, user_data):
     update.message.reply_text(
-    '''Добро пожаловать в Орехово Пицца!\nДля просмотра меню, акций,
-корзины или контактной информации\nнажмите соотвествующую кнопку:''',
-    reply_markup = pizza_main_menu_markup)
-
-    return 'state_choise'
+        'Добро пожаловать в Орехово Пицца!\nДля просмотра меню, акций\n'+
+        'корзины или контактной информации\nнажмите соотвествующую кнопку:',
+        reply_markup=pizza_main_menu_markup)
+    user_id =  update.message.from_user['id']
+    user_data[user_id] = {'id':user_id}
+    if 'cart' not in user_data[user_id].keys() :
+        user_data[user_id]['cart']=[]
+    print(user_data[user_id])
+    return 'pizzeria_main_menu_state'
 
 
 def menu_button_handler(bot, update, user_data):
     update.message.reply_text('Выберите категорию или вернитесь назад:',
-        reply_markup = ReplyKeyboardMarkup([['Пицца'],['Напитки'],['Прочее'],['Назад']]))
+                              reply_markup=ReplyKeyboardMarkup([['Пицца'],
+                                                                ['Напитки'],
+                                                                ['Прочее'],
+                                                                ['Назад']]))
     user_data['pizza'] = {}
     user_data['pizza']['menu_page'] = 0
-    return 'menu_state'
+    return 'pizzeria_menu_state'
 
 
-def back_from_menu_handler(bot, update, user_data): 
+def back_from_menu_handler(bot, update, user_data):
     update.message.reply_text(
-    '''Для просмотра меню, акций,\nкорзины или контактной информации
-нажмите соотвествующую кнопку:''',
-    reply_markup = pizza_main_menu_markup)
-    return 'state_choise'
+        'Для просмотра меню, акций,\nкорзины или контактной\n'+
+        'информации\nнажмите соотвествующую кнопку:',
+        reply_markup=pizza_main_menu_markup)
+    print(user_data[update.message.from_user['id']])
+    return 'pizzeria_main_menu_state'
 
 
 def print_pizza_menu(bot, update, user_data):
     pizza_photos = glob('images/pizza/pizza*.jp*g')
-    dct ={}
+    dct = {}
     for i in pizza_photos:
-        k = re.search(r'pizza(\d+)\.j',i)
-        dct[k.group(1)]=i
+        k = re.search(r'pizza(\d+)\.j', i)
+        dct[k.group(1)] = i
     pizza_num = user_data['pizza']['menu_page']
-    optional_buttons = [['Пред.','След.','Назад']]
+    optional_buttons = [['Пред.', 'След.', 'Назад']]
     if not pizza_num:
         optional_buttons[0].pop(0)
     elif pizza_num == 2:
         optional_buttons[0].pop(1)
-        
-    markup = [[str(i+1)] for i in range(pizza_num*5, min(5+pizza_num*5,14))]+optional_buttons
-    for i in range(pizza_num*5, min(5+pizza_num*5,14)):
+
+    markup = [[str(i + 1)] for i in range(pizza_num * 5, min(5 + pizza_num * 5, 14))] + optional_buttons
+    for i in range(pizza_num * 5, min(5 + pizza_num * 5, 14)):
         with open(dct[str(i)], 'rb') as f:
             bot.send_photo(chat_id=update.message.chat.id, photo=f)
-    update.message.reply_text('Выберите пиццу:', reply_markup = ReplyKeyboardMarkup(markup, resize_keyboard=True))
+    update.message.reply_text('Выберите пиццу:', reply_markup=ReplyKeyboardMarkup(markup, resize_keyboard=True))
 
 
 def pizza_category_handler(bot, update, user_data):
     print_pizza_menu(bot, update, user_data)
-    update.message.reply_text('Здесь будет выбор пиццы')
-    return 'pizza_choise'
+    return 'pizza_choise_state'
 
 
-def add_pizza_to_order_handler(bot, update, user_data):
+def add_pizza_to_cart_handler(bot, update, user_data):
     pizza_index = update.message.text
+    user_data[update.message.from_user['id']]['cart'].append(pizza_index)
     update.message.reply_text(f'Пицца №{pizza_index} добавленя в корзину')
     print_pizza_menu(bot, update, user_data)
 
 
 def change_menu_page_handler(bot, update, user_data):
     if update.message.text == 'Пред.':
-        user_data['pizza']['menu_page']-=1
+        user_data['pizza']['menu_page'] -= 1
         print_pizza_menu(bot, update, user_data)
     elif update.message.text == 'След.':
-        user_data['pizza']['menu_page']+=1
+        user_data['pizza']['menu_page'] += 1
         print_pizza_menu(bot, update, user_data)
     elif update.message.text == 'Назад':
-        menu_button_handler(bot, update, user_data)
-        return 'menu_state'
+        return menu_button_handler(bot, update, user_data)
+
+
+def checkout_handler(bot, update, user_data):
+    user_id =  update.message.from_user['id']
+    cart = user_data[user_id]['cart']
+    if not len(cart):
+        update.message.reply_text('Ваша корзина пуста', reply_markup=ReplyKeyboardMarkup([['Назад']]))
+    else:
+        for i in sorted(set(cart)):
+            update.message.reply_text(f'{i}x{cart.count(i)}, Цена: tbd\n')
+        update.message.reply_text('Для изменения заказа, нажмите соотвествующую кнопку', 
+            reply_markup = ReplyKeyboardMarkup([['Изменить заказ'],['Назад'],['Сделать заказ']]))
+#        markup = [[f'{i}x{cart.count(i)} -1'] for i in sorted(set(cart))]
+#        markup.extend([['Назад', 'Сделать заказ']])
+#        update.message.reply_text('Для уменьшения колличества позиций на 1\n'+
+#            'нажмите соотвествующую кнопку',
+#            reply_markup = ReplyKeyboardMarkup(markup, resize_keyboard=True))
+    return 'pizzeria_checkout_state'
+
+def change_cart_handler(bot, update, user_data):
+    user_id =  update.message.from_user['id']
+    cart = user_data[user_id]['cart']
+    markup = [[f'{i}x{cart.count(i)} -1'] for i in sorted(set(cart))]
+    markup.extend([['Назад']])
+    update.message.reply_text('Для уменьшения колличества позиций на 1\n'+
+        'нажмите соотвествующую кнопку',
+        reply_markup = ReplyKeyboardMarkup(markup, resize_keyboard=True))
+    return 'removeing_from_cart_state'
+
+
+def remove_from_cart_handler(bot, update, user_data):
+    item = re.search(r'(\w+)x\w\s*-1', update.message.text).group(1)
+    user_id = update.message.from_user['id']
+    cart = user_data[user_id]['cart']
+    cart.remove(item)
+    return change_cart_handler(bot, update, user_data)
+
+
+def back_from_checkout_handler(bot, update, user_data):
+    return back_from_menu_handler(bot, update, user_data)
+
+
+def order_pizza_handler(bot, update, user_data):
+    user_id =  update.message.from_user['id']
+    cart = user_data[user_id]['cart']
+    update.message.reply_text('Ваш заказ:',
+        reply_markup = ReplyKeyboardMarkup([['Назад'],['Отправить заказ']]))
+    for i in sorted(set(cart)):
+        update.message.reply_text(f'{i}x{cart.count(i)}, Цена: tbd\n')
+    return 'pizzeria_make_order_state'
+
+
+def back_from_order_handler(bot, update, user_data):
+    return checkout_handler(bot, update, user_data)
+
+
+def back_from_removing_from_cart_handler(bot, update, user_data):
+    return checkout_handler(bot, update, user_data)
+
+#def checks_before_order_handler(bot, update, user_data):
+#    phone_button = KeyboardButton('Телефон', request_contact=True)
+#    update.message.reply_text('Пожалуйста, заполните данные о себе:',
+#        reply_markup=ReplyKeyboardMarkup([[phone_button], ['Ввести имя']]))
+#    return 'test_phone_choise'
+
+
+def send_order_handler(bot, update, user_data):
+    user_id =  update.message.from_user['id']
+    bot.send_message(ADMIN_ID, f'Пользователь {user_id} сделал заказ:\n'+
+        f'{user_data[user_id]["cart"]}')
+    update.message.reply_text('Спасибо за заказ, вам позвонит оператор')
+    return pizza_main_menu_handler(bot, update, user_data)
 
 
 # Dummy handlers to test conversation handler
 def special_offers_handler(bot, update, user_data):
     update.message.reply_text('This is special offers button')
-    return 'end'
-
-
-def checkout_handler(bot, update, user_data):
-    update.message.reply_text('This is checkout button')
     return 'end'
 
 
@@ -105,5 +181,4 @@ def other_category_handler(bot, update, user_data):
     update.message.reply_text('Здесь будет прочее')
     return 'end'
 
-
-#def 
+# def
