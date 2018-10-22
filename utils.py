722918b997b412a840fb964bf6fc13b84384bb53
DanielK <rafikrafikrafik@yandex.ru>
2018-10-22 22:35:22 +0300

    Add tables to database: pizza, drinks, association for pizza, association for drinks; add func to get pizza object from db by id and to get string of pizza names from db to use in regex handler

--- utils.py
+++ utils.py
@@ -50,12 +50,15 @@ class Customer(Base):
         return f'<User {self.full_name} {self.email} {self.phone_number}>'
 
 
+
 class Order(Base):
     __tablename__ = 'orders'
     id = Column(Integer, primary_key = True)
     date = Column(DateTime)
     order = Column(Text)
     user_id = Column(ForeignKey('customers.id'))
+    pizza = relationship('Association', backref = 'order')
+    drink = relationship('Association_drinks', backref='order')
 
     def __init__(self, date = None, order = None, user_id = None):
         self.date = date
@@ -64,6 +67,52 @@ class Order(Base):
 
     def __repr__(self):
         return f'Order {self.id} - {self.order}'
+
+
+class Pizza(Base):
+    __tablename__ = 'pizzas'
+    id = Column(Integer, primary_key=True, autoincrement=False)
+    name = Column(String(100), unique = True)
+    price = Column(Integer)
+    order = relationship('Association', backref = 'pizza')
+
+    def __init__(self, id, name, price):
+        self.id = id
+        self.name = name
+        self.price = price
+
+    def __repr__(self):
+        return f'Pizza {self.name}: {self.price}'
+
+
+class Drink(Base):
+    __tablename__ = 'drinks'
+    id = Column(Integer, primary_key=True, autoincrement=False)
+    name = Column(String(100), unique = True)
+    price = Column(Integer)
+    order = relationship('Association_drinks', backref = 'drink')
+
+    def __init__(self, id, name, price):
+        self.id = id
+        self.name = name
+        self.price = price
+
+    def __repr__(self):
+        return f'Drink {self.name}: {self.price}'
+
+
+class Association(Base):
+    __tablename__='association'
+    order_id = Column(ForeignKey('orders.id'), primary_key=True)
+    pizza_id = Column(ForeignKey('pizzas.id'), primary_key=True)
+    extra_data = Column(String(100))
+
+
+class Association_drinks(Base):
+    __tablename__='association_drinks'
+    order_id = Column(ForeignKey('orders.id'), primary_key=True)
+    drink_id = Column(ForeignKey('drinks.id'), primary_key=True)
+    extra_data = Column(String(100))
 
 
 def create_tables():
@@ -90,11 +139,23 @@ def get_customer_id_by_phone(phone_number):
 
 
 def get_order_id_by_phone(phone_number):
-#    o = Order.query.filter(Order.customer.phone_number==phone_number).first()
+    #o = Order.query.filter(Order.customer.phone_number==phone_number).first()
     orders = Order.query.all()
     for i in orders:
         if i.customer.phone_number==phone_number:
             return i.id
 
+
+def get_pizza_by_id(id):
+    pizza = Pizza.query.filter(Pizza.id==id).first()
+    if not pizza:
+        return False
+    return pizza
+
+
+def pizza_names_for_regex_hendler():
+    pizza_names = [line.name for line in Pizza.query.all()]
+    string_of_names = "|".join(pizza_names)
+    return string_of_names
 
 
